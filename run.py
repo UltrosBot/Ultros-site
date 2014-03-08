943e05145619ea4bafab6d6ff5f6cbccc769bd63
Gareth Coles <gdude2002@gmail.com>
2014-03-08 15:32:01 +0000

    Some changes to make the contrib stuff more relevant

--- run.py
+++ run.py
@@ -23,4 +23,4 @@ static_class = static.StaticRoutes(app)
 application = app
 
 if __name__ == "__main__":
-    run(host='127.0.0.1', port=8080, server='cherrypy', reload=True)
+    run(host='127.0.0.1', port=8080, server='cherrypy')
