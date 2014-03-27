a82907145bd3b6b94236ac1146bf61c3f1ac4e12
Gareth Coles <gdude2002@gmail.com>
2014-03-27 20:13:06 +0000

    Keybase.io proofs

--- routes/static.py
+++ routes/static.py
@@ -12,9 +12,14 @@ class Routes(object):
         app.route("/static/<path:path>", ["GET", "POST"], self.static)
         app.route("/static/", ["GET", "POST"], self.static_403)
         app.route("/static", ["GET", "POST"], self.static_403)
+        app.route("/.well-known/keybase.txt", ["GET", "POST"],
+                  self.static_keybase)
 
     def static(self, path):
         return static_file(path, root="static")
 
     def static_403(self):
         abort(403, "You may not list the static files.")
+
+    def static_keybase(self):
+        return static_file("keybase.txt", root="static")
