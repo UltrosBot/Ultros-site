d369927d61a85a750c624419a51e5be33fd50093
Gareth Coles <gdude2002@gmail.com>
2015-10-30 18:34:15 +0000

    Make .well-known/ serve from a subdir in static/

--- routes/static.py
+++ routes/static.py
@@ -12,8 +12,10 @@ class Routes(object):
         route("/static/<path:path>", ["GET", "POST"], self.static)
         route("/static/", ["GET", "POST"], self.static_403)
         route("/static", ["GET", "POST"], self.static_403)
-        route("/.well-known/keybase.txt", ["GET", "POST"],
-              self.static_keybase)
+        route("/.well-known/<path:path>", ["GET", "POST"],
+              self.static_well_known)
+        route("/.well-known", ["GET", "POST"], self.static_403)
+        route("/.well-known/", ["GET", "POST"], self.static_403)
 
     def static(self, path):
         return static_file(path, root="static")
@@ -21,5 +23,5 @@ class Routes(object):
     def static_403(self):
         abort(403, "You may not list the static files.")
 
-    def static_keybase(self):
-        return static_file("keybase.txt", root="static")
+    def static_well_known(self, path):
+        return static_file(path, root="static/.well-known")
