d812c30df1e75281bc68bd4df03bbfd17c733265
Gareth Coles <gdude2002@gmail.com>
2017-05-21 13:12:35 +0100

    Some styling fixes

--- ultros_site/markdown.py
+++ ultros_site/markdown.py
@@ -14,8 +14,7 @@ class Markdown:
                 "fenced-code-blocks": {},
                 "header-ids": {},
                 "html-classes": {
-                    "table": "table is-bordered is-striped",
-                    "h1": "title"
+                    "table": "table is-bordered is-striped"
                 },
                 "target-blank-links": {},
                 "tables": {}
@@ -26,9 +25,13 @@ class Markdown:
 
     def add_classes(self, html):
         soup = BeautifulSoup(html, "html.parser")
-        headers = soup.find_all("h1")
+        h1 = soup.find_all("h1")
+        h2 = soup.find_all("h2")
 
-        for tag in headers:
+        for tag in h1:
             tag["class"] = "title"
+
+        for tag in h2:
+            tag["class"] = "subtitle"
 
         return str(soup)
